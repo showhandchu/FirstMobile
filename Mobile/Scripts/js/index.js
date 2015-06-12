bed85483b314a364236207da1ed04fb3b9cb1a70
showhand <showhandchu@outlook.com>
2015-06-12 16:14:17 +0800

    first

---
+++ Mobile/Scripts/js/index.js
@@ -0,0 +1,72 @@
+﻿var mobileModule = (function () {
+    var counter = 0;
+    return {
+        incrementCounter: function () {
+            return counter++;
+        },
+        resetCounter: function () {
+            console.log("counter value prior to reset: " + counter);
+            counter = 0;
+        },
+        setHeader: function (id) {
+            var source = $("#header-content").html();
+            var template = Handlebars.compile(source);
+            var html = template("");
+            $("#" + id).find("div:eq(0)").html(html);
+            $(".weather-icon").hover(
+                function () {
+                    $(this).addClass("temperature-icon").removeClass("weather-icon").html("<div class='tempature-text'>" + $(this).attr("temp") + "</div>");
+                }, function () {
+                    $(this).addClass("weather-icon").removeClass("temperature-icon").html("");
+                }
+            );
+        }
+    };
+})();
+
+$(document).on("pagechange", function (event, data) {
+    var pageId = data.toPage[0].id;
+    console.info(pageId);
+    switch (pageId) {
+        case "one":
+            //var source = $("#header-content").html();
+            //var template = Handlebars.compile(source);
+            //var html = template("");
+            //$("#one").find("div:eq(0)").html(html);
+            mobileModule.setHeader("one");
+
+            var contentType = "application/x-www-form-urlencoded; charset=utf-8";
+
+            if (window.XDomainRequest) //for IE8,IE9
+                contentType = "text/plain";
+
+            //$.ajax({
+            //    url: "http://localhost:8002/zh-tw/Home/GetPost",
+            //    data: { countrycode: "tw" },
+            //    type: "POST",
+            //    crossDomain: true,
+            //    //dataType: "json",
+            //    success: function (data) {
+            //        alert("Data from Server" + JSON.stringify(data));
+            //        console.info(data);
+            //    },
+            //    error: function (jqXHR, textStatus, errorThrown) {
+            //        alert("You can not send Cross Domain AJAX requests: " + errorThrown);
+            //        console.info(errorThrown);
+            //    }
+            //});
+            break;
+        case "two":
+            mobileModule.setHeader("two");
+            break;
+    }
+});
+
+//$(document).on("dialogcreate", function (event, ui) {
+//    var dialogId = event.target.id;
+//    //console.info(dialogId);
+//    switch (dialogId) {
+//        case "popup":
+//            break;
+//    }
+//});
---
+++ Mobile/Scripts/js/weather.js
@@ -0,0 +1,65 @@
+﻿$(function () {
+    //根據登入者所屬的國家顯示天氣資訊
+    var weatherCode = "23424971";
+    try {
+        switch (UserInfo.userCountryCode) {
+            case "cn":
+                weatherCode = "23424781";
+                break;
+            case "jp":
+                weatherCode = "23424856";
+                break;
+            case "kr":
+                weatherCode = "23424868";
+                break;
+            case "si":
+                weatherCode = "1062617";
+                break;
+            case "tw":
+                weatherCode = "23424971";
+                break;
+            case "vi":
+                weatherCode = "23424984";
+                break;
+        }
+    } catch (err) { }
+
+    $.ajax({
+        //台灣天氣
+        url: "https://query.yahooapis.com/v1/public/yql?q=select item from weather.forecast where woeid = '" + weatherCode + "' and u='c'&format=json",
+        dataType: "json",
+        success: function (data) {
+            //今天天氣
+            var today = data.query.results.channel.item.condition;
+            $("#header-content").find(".weather-icon:eq(0)").addClass("icon_" + today.code).attr("temp", today.temp);
+            //由於condition的date在formate時會有問題，因此就改由forecast[0].date做formate
+            today = data.query.results.channel.item.forecast[0];
+            $("#header-content").find(".weather-item div:eq(1)").html($.date(today.date));
+            mobileModule.setHeader("one");
+            ////明天天氣
+            //var tomorrow = data.query.results.channel.item.forecast[1];
+            //$("#tomorrow").addClass("icon_" + tomorrow.code).attr("temp", (parseInt(tomorrow.high) + parseInt(tomorrow.low)) / 2);
+            //$("#date2").html($.date(tomorrow.date));
+            ////後天天氣
+            //var aftertomorrow = data.query.results.channel.item.forecast[2];
+            //$("#aftertomorrow").addClass("icon_" + aftertomorrow.code).attr("temp", (parseInt(aftertomorrow.high) + parseInt(aftertomorrow.low)) / 2);
+            //$("#date3").html($.date(aftertomorrow.date));
+        }
+    });
+
+    //$("#today,#tomorrow,#aftertomorrow").hover(
+    //    function () {
+    //        $(this).addClass("temperature-icon").removeClass("weather-icon").html("<div class='tempature-text'>" + $(this).attr("temp") + "</div>");
+    //    }, function () {
+    //        $(this).addClass("weather-icon").removeClass("temperature-icon").html("");
+    //    }
+    //);
+});
+
+$.date = function (dateObject) {
+    var date = new Date(dateObject);
+    var day = date.getDate();
+    var month = date.getMonth() + 1;
+    var dateString = month + "/" + day;
+    return dateString;
+};
